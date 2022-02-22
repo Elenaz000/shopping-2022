0fd7ac0a4609f9e60686a60a6d34e4c15e8e8d0e
Elenaz000 <yz760@georgetown.edu>
2022-02-21 23:12:46 -0500

    Create second_bonus_challenge.py

---
+++ second_bonus_challenge.py
@@ -0,0 +1,118 @@
+
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] 
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" 
+
+#print(products)
+
+selected_products=[]
+total_price=0
+while True:
+
+    #ask for user input
+    product_id=input("Please input a product identifier: ")
+    #print(product_id) #>9
+    #print(type(product_id))
+    #type of input(product_id) 9 is a string 
+    
+    if product_id=="DONE":
+        break
+    else:
+        selected_products.append(product_id)
+
+
+
+
+
+
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv() 
+
+#TAX_RATE=0.0875
+TAX_RATE=os.getenv("TAX_RATE")
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+
+client = SendGridAPIClient(SENDGRID_API_KEY)
+print("CLIENT:", type(client))
+
+subject = "Your Receipt from the Green Grocery Store"
+
+html_content = "Hello World"
+
+html_content = "---------------------------------"
+html_content = "GREEN FOODS GROCERY"
+html_content = "WWW.GREEN-FOODS-GROCERY.COM"
+html_content = "---------------------------------"
+from datetime import datetime 
+html_content = "CHECK OUT AT: ", datetime.now()
+html_content = "---------------------------------"
+html_content = "SELECTED PRODUCTS: "
+
+matching_products=[]
+for product_id in selected_products:
+    matching_products = [x for x in products if str(x["id"]) == str(product_id)]
+    matching_product = matching_products[0]
+    total_price=total_price+matching_product["price"]
+    html_content = "... ", matching_product["name"], "(", to_usd(matching_product["price"]), ") "
+
+html_content = "---------------------------------"
+html_content = "SUBTOTAL: ", to_usd(total_price)
+html_content = "TAX: ", to_usd(total_price*TAX_RATE)
+html_content = "TOTAL: ", to_usd(total_price*(1+TAX_RATE))
+html_content = "---------------------------------"
+html_content = "THANKS, SEE YOU AGAIN SOON!"
+html_content = "---------------------------------"
+
+
+print("HTML:", html_content)
+
+message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
+
+try:
+    response = client.send(message)
+
+    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+    print(response.status_code) #> 202 indicates SUCCESS
+    print(response.body)
+    print(response.headers)
+
+except Exception as err:
+    print(type(err))
+    print(err)
